02ed224682de082e2a778b73a64cdf2e18a186f1
Hayden Bruin <bruinhayden@gmail.com>
2018-10-26 23:47:03 +1300

    Updating readme, setting levels & increased speed (wip)

--- src/App.js
+++ src/App.js
@@ -11,18 +11,36 @@ class App extends Component {
         const maxHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
         this.state = {
+
             targets: [],
-            targetsHit: 0,
+            targetsHitLevel: 0,
+            targetsHitTotal: 0,
             maxWidth: maxWidth,
             maxHeight: maxHeight,
+            roundLevel: 1,
             roundTimer: null
         }
+
+        this.levels = [
+            0, //offset index to 1
+            1000,
+            800,
+            600,
+            400,
+            200
+        ]
     }
 
     componentDidMount = () => {
+        this.setupRound();
+    }
+
+    setupRound = () => {
+        if(this.state.roundTimer) { clearInterval(this.state.roundTimer); }
+
         const timer = setInterval(() => {
             this.createTarget();
-        }, 1000);
+        }, this.levels[this.state.roundLevel]);
 
         this.setState({
             roundTimer: timer
@@ -35,8 +53,18 @@ class App extends Component {
 
         this.setState({
             targets: newTargets,
-            targetsHit: this.state.targetsHit + 1
+            targetsHitLevel: this.state.targetsHitLevel + 1,
+            targetsHitTotal: this.state.targetsHitTotal + 1
         })
+
+        if(this.state.targetsHitLevel === 10)
+        {
+            this.setState({
+                roundLevel: this.state.roundLevel + 1,
+                targetsHitLevel: 0
+            })
+        }
+
     }
 
     createTarget = () => {
@@ -58,6 +86,7 @@ class App extends Component {
             return (
                 <div className="app">
                     <h1>Sorry, You lose</h1>
+                    <h3>You did score {this.state.targetsHitTotal} though!</h3>
                 </div>
             )
         }
@@ -66,7 +95,9 @@ class App extends Component {
             return (
                 <div className="app">
                     <div className="scoreboard">
-                        <div className="score">Targets Hit: {this.state.targetsHit}</div>
+                        <div className="level">level: {this.state.roundLevel}</div>
+                        <div className="score">Level Targets Hit: {this.state.targetsHitLevel}</div>
+                        <div className="score">Total Targets Hit: {this.state.targetsHitTotal}</div>
                         <div className="score">Targets Active: {this.state.targets.length}/10</div>
                     </div>
                     <Targets hitTarget={this.hitTarget} targets={this.state.targets} />
